b95ae1671fc6c16913066de0aaf643a716722525
Sthefanny Gonzaga <sthefannygonzaga@gmail.com>
2022-08-29 16:45:57 -0300

    TDBDN-75: Xcode project created

---
+++ Bora.Today/Bora_TodayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bora_TodayApp.swift
+//  Bora.Today
+//
+//  Created by Sthefanny Gonzaga on 29/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Bora_TodayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
