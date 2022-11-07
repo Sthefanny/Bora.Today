9f4df9fe96f8660fa217ba71a8e2a14140ea219c
Jessica Akemi Meguro <jessicameguro@gmail.com>
2022-11-07 17:04:44 -0300

    Adição do Botão de Criar e Inicio da View

---
+++ Bora.Today/Modules/Local Experience/Components/LocalBannerView.swift
@@ -0,0 +1,20 @@
+//
+//  LocalBannerView.swift
+//  Bora.Today
+//
+//  Created by Jessica Akemi Meguro on 07/11/22.
+//
+
+import SwiftUI
+
+struct LocalBannerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LocalBannerView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocalBannerView()
+    }
+}
---
+++ Bora.Today/Modules/Local Experience/LocalsView.swift
@@ -0,0 +1,20 @@
+//
+//  LocalsView.swift
+//  Bora.Today
+//
+//  Created by Jessica Akemi Meguro on 07/11/22.
+//
+
+import SwiftUI
+
+struct LocalsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LocalsView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocalsView()
+    }
+}
