c462423bb597902f103374f3456d3a7fadc362f8
Sthefanny Gonzaga <sthefannygonzaga@gmail.com>
2022-11-08 14:10:28 -0300

    bt-147: calendar and events working

--- Bora.Today/Common/Models/EventModel.swift
+++ Bora.Today/Common/Models/EventModel.swift
@@ -9,6 +9,8 @@ import Foundation
 
 struct EventModel: Identifiable {
     let id = UUID()
+    let initialDate: String
+    let finalDate: String
     let date: String
     let startHour: String
     let endHour: String
@@ -23,7 +25,9 @@ extension EventModel {
     static var example1: EventModel {
 
         EventModel(
-            date: "27/10/2022",
+            initialDate: "2022-11-02T08:00:00-0300",
+            finalDate: "2022-11-02T09:00:00-0300",
+            date: "02/11/2022",
             startHour: "08:00",
             endHour: "09:00",
             name: "Hamburguer da Stel",
@@ -34,11 +38,26 @@ extension EventModel {
     static var example2: EventModel {
 
         EventModel(
+            initialDate: "2022-11-05T11:00:00-0300",
+            finalDate: "2022-11-05T13:00:00-0300",
             date: "20/10/2022",
             startHour: "13:00",
             endHour: "15:00",
             name: "Festa da Aninha",
             location: "Minha casa"
+        )
+    }
+    
+    static var example3: EventModel {
+
+        EventModel(
+            initialDate: "2022-11-10T11:00:00-0300",
+            finalDate: "2022-11-10T13:00:00-0300",
+            date: "20/10/2022",
+            startHour: "13:00",
+            endHour: "15:00",
+            name: "Festa da Jessie",
+            location: "Casa da Jessie"
         )
     }
 }
