9b5cdb67d8a522cd3315ae9e54b0106a18e17550
anastege <analuisastege@gmaiol.com>
2022-11-10 21:53:42 -0300

    Created Settings Profile Components

--- Bora.Today/Modules/Profile/Settings/SettingsProfileView.swift
+++ Bora.Today/Modules/Profile/Settings/SettingsProfileView.swift
@@ -8,24 +8,68 @@
 import SwiftUI
 
 struct SettingsProfileView: View {
+    // MARK: - PROPERTIES
     
     @Binding var backgroundPrivateColor: Color
     
+    // MARK: - BODY
     var body: some View {
         VStack{
+            // MARK: - HEADER
             HeaderView(headerTitle: "Configurações")
             VStack {
-                HStack {
-                    Text("Conexões")
-                        .font(.appHeadline)
-                    .foregroundColor(.appBlack)
-                    Spacer()
-                }
-            }
+                // MARK: - SECTION CONFIGURAÇÕES
+                SettingsProfileLabelView(labelText: "Conexões")
+                    GroupBox(content: {
+                        
+                        SettingsProfileRowView(title: "Idioma", imageName: "", linkLabel: "Portugues", linkDestination: "google")
+                        HStack{
+                            Text("Idioma")
+                                .font(.appButtonText)
+                                .foregroundColor(.appBlack)
+                            Spacer()
+                            Text("Português")
+                                .font(.appSubheadline)
+                                .foregroundColor(.appBlueButton)
+                        }
+                    }, label: {
+                    })
+                    .groupBoxStyle(SettingsGroupBox())
+                    // MARK: - SECTION SOBRE
+                SettingsProfileLabelView(labelText: "Sobre")
+                    GroupBox(content: {
+                        VStack{
+                            HStack{
+                                Text("BoraCast")
+                                    .font(.appButtonText)
+                                    .foregroundColor(.appBlack)
+                                Spacer()
+                                Image(systemName: "chevron.right")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .foregroundColor(.appBlueButton)
+                                    .frame(width: 13, height: 13)
+                            }
+                            Divider()
+                                .frame(height:1)
+                                .overlay(Color.appWhite)
+                                .padding(.vertical,0)
+                        }
+                    }, label: {
+                    })
+                    .groupBoxStyle(SettingsGroupBox())
+                    
+                    // MARK: - SECTION 1
+                    
+                    // MARK: - SECTION 2
 
+            }
+            .padding(.horizontal, AppConfig.safeAreaHorizontal)
         }
     }
 }
+
+// MARK: - PREVIEW
 
 struct SettingsProfileView_Previews: PreviewProvider {
     static var previews: some View {
@@ -53,5 +97,15 @@ private struct HeaderView: View {
             Text(headerTitle ?? "")
                 .font(.appTitle2)
         } .padding(.horizontal, AppConfig.safeAreaHorizontal)
+    }
+}
+
+struct SettingsGroupBox: GroupBoxStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.content
+            .frame(maxWidth: .infinity)
+            .padding()
+            .background(RoundedRectangle(cornerRadius: 10).fill(Color.appLightGray))
+            .overlay(configuration.label.padding(.leading, 4), alignment: .topLeading)
     }
 }
