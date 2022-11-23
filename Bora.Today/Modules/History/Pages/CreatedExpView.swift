073ff01c65e8a50365aa13a3b98b6287dd8900a6
Sthefanny Gonzaga <sthefannygonzaga@gmail.com>
2022-11-23 16:49:07 -0300

    Connecting to people

---
+++ Bora.Today/Modules/History/Pages/CreatedExpView.swift
@@ -0,0 +1,20 @@
+//
+//  CreatedExpView.swift
+//  Bora.Today
+//
+//  Created by Sthefanny Gonzaga on 23/11/22.
+//
+
+import SwiftUI
+
+struct CreatedExpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreatedExpView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreatedExpView()
+    }
+}
